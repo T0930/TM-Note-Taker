4437f6dc868c5cfc94d725a3c0cdbb36f756478d
Tom M <T0930@protonmail.com>
2022-01-10 15:39:22 -0700

    updated apirouts.js and htmlrouts.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,2 +1,24 @@
 
+const fs = require('fs');
 const app = require('express').Router()
+const db = require("../db/db.json");
+
+app.get('/api/notes', function (req, res){
+    db = JSON.parse(fs.readFileSync('./db/db.json')) || []
+    console.log('get', db)
+    res.json(db)
+})
+
+app.post('/api/notes', function (req, res){
+    let newNote = {
+        id: Math.floor(math.random() * 1000),
+        title: req.params.title,
+        text: req.params.text
+    }
+    db.push(newNote)
+    fs.writeFileSync('./db/db.json', JSON.stringify(db),function(err){
+        if(err) throw err;
+    }) 
+    console.log('post', db)
+    res.json(db)
+})
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,3 +1,17 @@
 
 const app = require('express').Router()
 const path = require('path')
+
+app.get('/notes', function(req, res){
+     res.sendFile(path.join(__dirname,"../public/notes.html"))
+});
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+});
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+});
+
+module.exports = app;
