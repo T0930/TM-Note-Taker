459cf9502154cedc05ed1f941cae067a7962a49f
Tom M <T0930@protonmail.com>
2022-01-13 14:08:57 -0700

    update heroku

--- server.js
+++ server.js
@@ -9,16 +9,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoute)
+app.use('/api', apiRoute);
+app.use(require('./routes/htmlroutes'));
 
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
 app.listen(PORT, () =>
     console.log(`Listening at http://localhost:${PORT}`)
